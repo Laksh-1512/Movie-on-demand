8631b391094aeb92edbc275c4be0d3d1d6dac1d3
laksh2444 <lakshpaul1512@gmail.com>
2024-04-12 17:15:23 +0530

    security increase

--- src/components/Gptsearchbar.js
+++ src/components/Gptsearchbar.js
@@ -12,6 +12,7 @@ const Gptsearchbar = () => {
 
   const searchMovieTMDB=async(movie)=>{
     const data=await fetch('https://api.themoviedb.org/3/search/movie?query='+movie+'&include_adult=false&language=en-US&page=1', options);
+    
     const json=await data.json();
     return json.results; //return promise
   }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,4 +10,4 @@ export const options = {
 };
 export const poster="https://image.tmdb.org/t/p/w500";
 export const bg_url="https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_large.jpg";
-export const API_KEY="AIzaSyB7ohbyZkKSQRP8Jl4mgP7hBLgX2ioe_8w";
+export const API_KEY=process.env.REACT_APP_API_KEY;
